c761a1bacbcdc6c0360b83f7661252878289e81a
Vivek Jayakumar <vivek.l@go-jek.com>
2022-01-30 11:41:46 +0400

    Updated UI Test

--- CreditScore/CreditScoreUITests/CreditScoreUITestsLaunchTests.swift
+++ CreditScore/CreditScoreUITests/CreditScoreUITestsLaunchTests.swift
@@ -9,22 +9,34 @@ import XCTest
 
 class CreditScoreUITestsLaunchTests: XCTestCase {
 
+    var app: XCUIApplication!
+
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
+    
 
     override func setUpWithError() throws {
         continueAfterFailure = false
-    }
-
-    func testLaunch() throws {
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launchArguments.append("UITesting")
         app.launch()
-
-        let label = app.staticTexts["200"]
-        XCTAssertTrue(label.waitForExistence(timeout: 10))        
+        sleep(1)
     }
-
     
+    func testLaunch() throws {
+        let label = app.staticTexts["200"]
+        XCTAssertTrue(label.waitForExistence(timeout: 10))
+    }
+    
+}
+
+extension XCUIElement {
+    func tap(wait: Int, test: XCTestCase) {
+        if !isHittable {
+            test.expectation(for: NSPredicate(format: "hittable == true"), evaluatedWith: self, handler: nil)
+            test.waitForExpectations(timeout: TimeInterval(wait), handler: nil)
+        }
+        tap()
+    }
 }
